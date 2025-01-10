520e123231f28047c1cc02f8002ff3a1f322a5dc
sungyeong98 <am25227903@gmail.com>
2025-01-10 15:46:53 +0900

    feat: Next.js-11강 완료

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +26,22 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-[100dvh] border-[5px]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-[100dvh] border-[5px]`}
       >
-        <head className="border-[2px] border-[red]">
-          <a href="/">홈</a>
-          <a href="/about">소개</a>
-        </head>
-        <main className="border-[2px] border-[blue]">{children}</main>
-        <footer className="border-[2px] border-[pink]">Copyright 2025.</footer>
+        <header className="border-[2px] border-[red] p-5">
+          <div className="flex gap-2">
+            <Link href="/">홈</Link>
+            <Link href="/about">소개</Link>
+          </div>
+        </header>
+
+        <main className="flex-grow border-[2px] border-[blue] p-5">
+          {children}
+        </main>
+
+        <footer className="border-[2px] border-[pink] p-5">
+          Copyright 2025.
+        </footer>
       </body>
     </html>
   );
